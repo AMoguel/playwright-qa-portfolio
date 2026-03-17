a6bc3b3ed7957bc9799245e45ce43bc18e95f729
Andre Moguel <mr.amoguel@gmail.com>
2026-03-17 13:21:47 -0600

    Removed registration flow from login flow

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -44,16 +44,3 @@ test.describe('User login', () => {
 })
 
 
-test.describe('Registration flow', () => {
-    
-    test('submitting registration', async ({page}) => {
-        await page.goto('https://practice.expandtesting.com/register')
-        await page.locator('#username').fill('autotest') 
-        await page.locator('#password').fill('Kazama122!')   
-        await page.locator('#confirmPassword').fill('Kazama122!')
-        await page.getByRole('button', { name: "Register"}).click()
-    
-
-
-    })
-})
