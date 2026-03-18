282cf31f5ea1d988237ec8fdc21515bfcabda9b5
Andre Moguel <mr.amoguel@gmail.com>
2026-03-17 18:25:19 -0600

    Added negative test cases

--- tests/Registration.spec.ts
+++ tests/Registration.spec.ts
@@ -1,10 +1,16 @@
 import{test, expect} from '@playwright/test'
 
 
+test.beforeEach(async ({page}) => {
+    await page.goto('https://practice.expandtesting.com/register')
+})
+
+
 test.describe('Registration flow', () => {
-    
-    test('submitting registration', async ({page}) => {
-        await page.goto('https://practice.expandtesting.com/register')
+
+
+
+    test('Successfull registration', async ({page}) => {
         await page.locator('#username').fill('autotest') 
         await page.locator('#password').fill('Kazama122!')   
         await page.locator('#confirmPassword').fill('Kazama122!')
@@ -13,4 +19,31 @@ test.describe('Registration flow', () => {
 
 
     })
+
+    test('Entering wrong confimation password', async ({page}) => {
+        await page.locator('#username').fill('autotest') 
+        await page.locator('#password').fill('Kazama122!')  
+        await page.locator('#confirmPassword').fill('password122!')
+        await page.getByRole('button', { name: "Register"}).click()
+        await expect(page.locator('#flash')).toHaveText('Passwords do not match.')
+
+
+
+    })
+
+    test('Missing username', async ({page}) => {
+        await page.locator('#password').fill('Kazama122!')  
+        await page.locator('#confirmPassword').fill('password122!')
+        await page.getByRole('button', { name: "Register"}).click()
+        await expect(page.locator('#flash')).toHaveText('All fields are required.')
+
+    })
+
+    test('Missing passwords', async ({page}) => {
+        await page.locator('#username').fill('autotest') 
+        await page.getByRole('button', { name: "Register"}).click()
+        await expect(page.locator('#flash')).toHaveText('All fields are required.') 
+    })
+
+
 })
